b611a9976614c93bcd95343002b6733646b5d932
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-10-11 17:26:27 -0700

    Set up test server

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ require('dotenv').config();
 // Initialize Express app
 const app = express();
 
+// Define homepage for app
+app.route('/').get(function (req, res) {
+  res.sendFile(process.cwd() + 'index.html');
+});
+
+// Use port 5000 for testing
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
+});

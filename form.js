e97a3aaa8340bf4708aeff9c0664428eb79480a2
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-10-11 17:09:21 -0700

    Add client-side form funcionality

---
+++ form.js
@@ -0,0 +1,21 @@
+// Client-side form handling
+
+const form = document.getElementById('contact-form');
+
+// Add event listener to the form
+const formEvent = form.addEventListener('submit', event => {
+  event.preventDefault();
+  let mail = new FormData(form);
+  sendMail(mail);
+})
+
+// Create sendMail function to submit the form data
+const sendMail = (mail) => {
+  fetch('http://localhost:5000', {
+    method: "post",
+    body: mail,
+  })
+  .then((response) => {
+    return response.json();
+  })
+}

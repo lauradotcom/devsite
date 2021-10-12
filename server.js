ec0a53705884ea0da2383766140234f79bad81cf
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-10-11 17:31:49 -0700

    Set up Nodemailer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const multiparty = require('multiparty');
+const SMTPConnection = require('nodemailer/lib/smtp-connection');
 require('dotenv').config();
 
 // Initialize Express app
@@ -15,4 +16,23 @@ app.route('/').get(function (req, res) {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
+});
+
+// Set up Nodemailer transporter object
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+// Verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready');
+  }
 });

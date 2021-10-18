60822dbcc70a0c5b4b73dfa9320d4c0422517939
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-10-18 15:05:10 -0700

    Add funcionality to detect scrolling and apply/remove class

--- public/js/showonscroll.js
+++ public/js/showonscroll.js
@@ -1,2 +1,15 @@
 // Trigger animations on scroll
 
+const articles = document.querySelector('article');
+const container = document.getElementById('portfolio');
+container.addEventListener('scroll', function () {
+  articles.forEach((e, i) => {
+    const top = (e.getBoundingClientRect().top);
+    if ( top == 0) {
+      e.classList.add('is-inview');
+    } else {
+      e.classList.remove('is-inview');
+    }
+  })
+})
+container.dispatchEvent(newCustomEvent('scroll'));
